e59ea7d0a4acb44ae619d6a14fdd851bf8594a2f
Doug Bromley <doug.bromley@gmail.com>
2014-06-06 23:20:04 +0100

    Initial code commit as moving code from current class and cleaning up

---
+++ FreeagentApiV2.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * An API wrapper for the FreeAgent v2 REST API
+ * that uses OAuth for authentication of
+ * API requests.
+ *
+ * @author Doug Bromley <doug@tintophat.com>
+ * @copyright Doug Bromley
+ * @link https://github.com/OdinsHat/PHP-FreeAgent-API-v2
+ * @license MIT
+ */
+
+class FreeagentApiV2
+{
+    public function __construct()
+    {
+
+    }
+
+    public function refreshToken()
+    {
+
+    }
+
+    public function loadToken()
+    {
+
+    }
+}
