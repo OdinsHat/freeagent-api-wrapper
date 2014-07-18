8c4e9122c8f54ec5372ae3cb53d7f941b31ecc0e
Doug Bromley <doug.bromley@gmail.com>
2014-07-18 21:57:08 +0100

    Adding some of the methods to contacts class

--- FreeagentApi/Contacts.php
+++ FreeagentApi/Contacts.php
@@ -5,6 +5,50 @@ namespace OdinsHat\FreeAgentApiV2\FreeagentApi;
 class Contacts extends FreeAgentApiV2
 {
 
+    public function all()
+    {
+
+    }
+
+    public function active()
+    {
+
+    }
+
+    public function clients()
+    {
+
+    }
+
+    public function suppliers()
+    {
+
+    }
+
+    public function active()
+    {
+
+    }
+
+    public function get_contact()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
 }
 
 ?>
